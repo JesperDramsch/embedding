554b7bf063d725d3041cb555ec6cf2a947e9558a
Jesper Dramsch <jesper@dramsch.net>
2018-06-09 13:13:24 +0200

    More variability

--- classifier_choice.py
+++ classifier_choice.py
@@ -1,7 +1,7 @@
 from sklearn import datasets, manifold, decomposition, ensemble, discriminant_analysis
 from sklearn.pipeline import Pipeline
 """
-Please chose a classifier:
+Please choose a classifier:
 pca = Principal Component Analysis
 isomap = Isomapping
 lle = Local Linear Embedding
@@ -14,30 +14,30 @@ tsne = t-distributed stochastic neighbor embedding
 """
 
 
-def classifier_choice(method='tsne'):
+def classifier_choice(method='tsne', neighbors=30, dimensions=2):
     X = data.copy()
     if method in "tsne":
-        return manifold.TSNE(n_components=2, init='pca', random_state=0)
+        return manifold.TSNE(n_components=dimensions, init='pca')
     elif method in "pca":    
-        return decomposition.TruncatedSVD(n_components=2)
+        return decomposition.TruncatedSVD(n_components=dimensions)
     elif method in "isomap":
-        return manifold.Isomap(n_neighbors=30, n_components=2)
+        return manifold.Isomap(n_neighbors=neighbors, n_components=dimensions)
     elif method in "lle":
-        return manifold.LocallyLinearEmbedding(n_neighbors=30, n_components=2, method='standard')
+        return manifold.LocallyLinearEmbedding(n_neighbors=neighbors, n_components=dimensions, method='standard')
     elif method in "mlle":
-        return manifold.LocallyLinearEmbedding(n_neighbors=30, n_components=2, method='modified')
+        return manifold.LocallyLinearEmbedding(n_neighbors=neighbors, n_components=dimensions, method='modified')
     elif method in "hlle":
-        return manifold.LocallyLinearEmbedding(n_neighbors=30, n_components=2, method='hessian')
+        return manifold.LocallyLinearEmbedding(n_neighbors=neighbors, n_components=dimensions, method='hessian')
     elif method in "ltsa":
-        return manifold.LocallyLinearEmbedding(n_neighbors=30, n_components=2, method='ltsa')
+        return manifold.LocallyLinearEmbedding(n_neighbors=neighbors, n_components=dimensions, method='ltsa')
     elif method in "mds":
-        return manifold.MDS(n_components=2, n_init=1, max_iter=100)
+        return manifold.MDS(n_components=dimensions, n_init=1, max_iter=100)
     elif method in "trees":
-        trees = ensemble.RandomTreesEmbedding(n_estimators=200, random_state=0, max_depth=5)
-        pca = decomposition.TruncatedSVD(n_components=2)
+        trees = ensemble.RandomTreesEmbedding(n_estimators=200, max_depth=5)
+        pca = decomposition.TruncatedSVD(n_components=dimensions)
         return Pipeline([('Random Tree Embedder',trees),('PCA',pca)])
     elif method in "spectral":
-        return manifold.SpectralEmbedding(n_components=2, random_state=0, eigen_solver="arpack")
+        return manifold.SpectralEmbedding(n_components=dimensions, eigen_solver="arpack")
     else:
         print('Please use valid method')
 
